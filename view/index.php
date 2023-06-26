42230dbb5b8fa628f9dc516f9de405d16ef008a6
ErnestoSil <ernestosilfl@gmail.com>
2023-06-26 10:44:41 -0600

    Proyecto sistema de registro documental

--- view/index.php
+++ view/index.php
@@ -83,10 +83,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="../plantilla/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">SISTEMA TRAMITE</span>
-    </a>
+
 
     <!-- Sidebar -->
     <div class="sidebar">
